d2b07b3542cce3ce4724933a9ef41cd3072b2d27
Steven Figo <derkora.27@gmail.com>
2023-12-04 01:20:16 +0700

    iya karena while

--- GPT2/main.py
+++ GPT2/main.py
@@ -1,36 +1,33 @@
 from transformers import pipeline, set_seed
 import streamlit as st
 
-def askGPT(chat):
-  path = "akahana/gpt2-indonesia"
-  generator = pipeline('text-generation', 
-                      model=path)
-  set_seed(42)
+if "messages" not in st.session_state:
+  st.session_state.messages = []
+
+for message in st.session_state.messages:
+  with st.chat_message(message["role"]):
+      st.markdown(message["content"])
+          
+prompt = st.chat_input("Say something")
+if prompt:
+  with st.chat_message(name = "user"):
+    st.markdown(prompt) 
+
+  st.session_state.messages.append({"role": "user", "content": prompt})
   
-  preds = generator(chat, 
-                    max_length=64,
-                    num_return_sequences=3)
-  for data in preds:
-    print(data)
-     
-    with st.chat_message(name = "assistant"): 
-      st.markdown(data)
+path = "akahana/gpt2-indonesia"
+generator = pipeline('text-generation', 
+                    model=path)
+set_seed(42)
 
-    st.session_state.messages.append({"role": "assistant", "content": data})
+preds = generator(prompt, 
+                  max_length=64,
+                  num_return_sequences=3)
+for data in preds:
+  print(data)
     
-while True:
-  if "messages" not in st.session_state:
-    st.session_state.messages = []
+  with st.chat_message(name = "assistant"): 
+    st.markdown(data)
 
-  for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
-            
-  prompt = st.chat_input("Say something")
-  if prompt:
-    with st.chat_message(name = "user"):
-      st.markdown(prompt) 
-
-    st.session_state.messages.append({"role": "user", "content": prompt})
-  askGPT(prompt)
+  st.session_state.messages.append({"role": "assistant", "content": data})
   
